a21c3c2abe25bad1929db557ca602be2f4ebebe5
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-10-22 12:45:38 +0530

    Added exception handling code

--- Examples/ExceptionHandling.py
+++ Examples/ExceptionHandling.py
@@ -1,9 +1,21 @@
 
 
-try:
-    uservalue_str = input ("Type the value : ")
-    uservalue_int = int(uservalue_str)
-    print("Succssfull Converted to int : " , uservalue_int)
+#try:
+ #   uservalue_str = input ("Type the value : ")
+ #   uservalue_int = int(uservalue_str)
+ #   print("Succssfull Converted to int : " , uservalue_int)
 
-except ValueError:
-    print("Invalid Input . Please  eter a valid Integer")
+#except ValueError:
+ #   print("Invalid Input . Please  eter a valid Integer")
+
+
+inputv = input("Enter the value : ")
+
+try:
+    inputv_int = int(inputv)
+except  :
+    print("EROR")
+else   :
+    print("Integer number is entered")
+
+
