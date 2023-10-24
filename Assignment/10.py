5b48e2184153ad7ecda4a87bdd3033c355053a01
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-10-24 12:48:37 +0530

    Create 10.py

---
+++ Assignment/10.py
@@ -0,0 +1,12 @@
+"""
+Which of the following are legal python variable names? If a name is
+
+not legal, state the reason.
+ds1, 1ds, your_name, her-name, pass, _pass
+
+"""
+"""
+
+ Legal Variables = ds1 , your_name , pass , _pass
+ Not Supported = 1ds, her-name , 
+"""
