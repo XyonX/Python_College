b16e388adef2887bbeff1c50d055bf431c80a2cb
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-10-24 12:38:47 +0530

    Create 8.py

---
+++ Assignment/8.py
@@ -0,0 +1,34 @@
+"""
+
+Write the features of list data structure. Give an example to create list in
+python
+
+"""
+
+"""
+
+The Features of list data types is 
+    1. Dynamic in size so that we dont have to be worried about the size
+    2. List is mutable so we can modify list after creating its makes it flexible 
+    3. Heterogeneous Elements type adding is supported so a list can contain multiple type data
+    4. List is ordered similar to array so elements can be accessed via their index 
+
+
+"""
+
+# Example of creation of creation of a list
+
+
+demo_list = [10, 20, 30 , "Ayesha"]
+print(demo_list)
+print(demo_list[0])
+
+
+"""
+C:\Projects\College\Python\venv\Scripts\python.exe C:/Projects/College/Python/Assignment/8.py
+[10, 20, 30, 'Ayesha']
+10
+
+Process finished with exit code 0
+
+"""
