a37d7ee8157381083441414ca31b43fde6612026
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-10-24 12:41:01 +0530

    Create 9.py

---
+++ Assignment/9.py
@@ -0,0 +1,17 @@
+
+"""
+Write a python code to sort and display all elements of a list.
+
+"""
+
+#Declaration of a demo int list
+demo_list = [6,4,3,10,0,9,1 ]
+
+#Printing all the elements before sorting
+print( "Printing all the elements before sorting: " + str(demo_list))
+
+#Sorting the list
+demo_list.sort()
+
+#Printing all the elements after sorting
+print( "Printing all the elements after sorting : " + str(demo_list))
